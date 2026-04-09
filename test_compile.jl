939fb994a3d697ac10a3fb302f0d8e6db116b008
NinaOmejc <nina.omejc@gmail.com>
2026-04-09 16:54:05 +0200

    remove test scripts

--- test_compile.jl
+++
@@ -1,7 +0,0 @@
-using ENEEGMA
-println("ENEEGMA loaded successfully!")
-println("Types available:")
-println("  - ParamTransform: $(ParamTransform)")
-println("  - ParamReparamTransform: $(ParamReparamTransform)")
-println("  - ReparamSpec: $(ReparamSpec)")
-println("  - OptLogEntry: $(OptLogEntry)")
--- test_settings_fix.jl
+++
@@ -1,6 +0,0 @@
-using ENEEGMA
-
-println("Testing settings creation...")
-settings = create_default_settings()
-println("PASS: Settings created successfully")
-println("LossSettings fields: $(length(fieldnames(LossSettings)))")
